ac107b369373e40cc5048d478cc3b293ac09a1a0
Dung Le <dung.le@audiencemedia.com>
2016-06-17 22:41:19 +0700

    Initial Commit

---
+++ UberClone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UberClone
+//
+//  Created by Dung Le on 6/17/16.
+//  Copyright © 2016 Dung Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UberClone/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UberClone
+//
+//  Created by Dung Le on 6/17/16.
+//  Copyright © 2016 Dung Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
